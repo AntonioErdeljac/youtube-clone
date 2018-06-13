89952ef5cbb330ec1053992f6b27859492108e8c
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2018-06-13 02:24:24 +0200

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import '../resources/scss/style.scss';
+
+ReactDOM.render(
+  <p>Hello World</p>,
+  document.getElementById('root'),
+);
