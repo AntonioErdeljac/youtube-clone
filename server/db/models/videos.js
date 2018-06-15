32868e2c3addfc189b226dcb94dcb467b69e21eb
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2018-06-15 02:41:06 +0200

    Added create & isValid method to Videos model

--- server/db/models/videos.js
+++ server/db/models/videos.js
@@ -10,4 +10,21 @@ const Videos = mongoose.model('videos', new Schema({
   },
   description: String,
   file: String,
+  views: {
+    type: Number,
+    default: 0,
+  },
+  upvotes: {
+    type: Number,
+    default: 0,
+  }
 }, { timestamps: true }));
+
+module.exports.isValid = values => !Videos(values).validateSync();
+
+module.exports.create = (values) => {
+  const video = _.omit(values, ['_id']);
+
+  return Videos(video).save()
+    .then(() => video);
+};
