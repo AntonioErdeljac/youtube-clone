1a80923be7aeae7258e026d48f93cb0cd2a71b09
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2018-06-23 23:25:57 +0200

    Added updateById to comments

--- server/controllers/comments/index.js
+++ server/controllers/comments/index.js
@@ -1,5 +1,7 @@
 const create = require('./create');
 const getByVideo = require('./getByVideo');
+const updateById = require('./updateById');
 
 module.exports.create = create;
 module.exports.getByVideo = getByVideo;
+module.exports.updateById = updateById;
--- server/routes/api/comments.js
+++ server/routes/api/comments.js
@@ -5,4 +5,6 @@ const authentication = require('../authentication');
 module.exports = (router) => {
   router.post(paths.api.v1.COMMENTS, authentication.required, comments.create);
   router.get(paths.api.v1.COMMENTS, authentication.optional, comments.getByVideoId);
+
+  router.patch(paths.api.v1.COMMENT_ID, authentication.required, comments.updateById);
 };
